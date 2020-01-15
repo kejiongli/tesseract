aea9da5c2043168858a195825dbe51c9eb4d9ad9
Kejiong <kejiong.li@gmail.com>
2020-01-15 00:39:39 +0000

    improved

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-1. Install `tesserocr` package (see `https://pypi.org/project/tesserocr/` for more details)
-2. Download data file `https://github.com/tesseract-ocr/tessdata/blob/master/eng.traineddata` and save the file in `tessdata`. 
-You may find this folder in the virtual environment folder
+1. Create virtual environment: `python -m venv venv`
+1. Install `pillow` package 
+   ```cmd
+    pip install -r requirements.txt
+    ```
+1. Download `tesserocr` whl file from [here](https://github.com/simonflueckiger/tesserocr-windows_build/releases) and install using pip
+    ```$xslt
+    pip install <downloadfile>.whl
+    ```
+1. Download data file `https://github.com/tesseract-ocr/tessdata/blob/master/eng.traineddata` and save the file in `$PATH_TO_PYTHON/tessdata`, 
+where `$PATH_TO_PYTHON` can be obtained by run cmd `where python`
--- run_local.py
+++ run_local.py
@@ -1,7 +1,78 @@
 import glob
+import json
 import os
 
-from tesserocr import PyTessBaseAPI, PSM, OEM
+from tesserocr import PyTessBaseAPI, PSM, OEM, RIL, iterate_level
+
+
+def func1(img: str):
+    with PyTessBaseAPI(psm=PSM.AUTO, oem=OEM.LSTM_ONLY) as api:
+        api.SetImageFile(img)
+
+        text = api.GetUTF8Text()
+        data = {'text': text, 'lines': []}
+
+        lines = api.GetTextlines(raw_image=True, raw_padding=5)
+        # lines = api.GetComponentImages(level=RIL.TEXTLINE, text_only=False, raw_image=True, raw_padding=5)
+        for idx, [image, bbox, bid, pid] in enumerate(lines):
+            # with open(os.path.join(odir, f'line-{idx}.jpg'), 'wb') as f:
+            #     image.save(f)
+            api.SetImage(image)
+            text = api.GetUTF8Text()
+
+            line_data = dict(bbox=bbox, bid=bid, pid=pid, text=text)
+
+            # words = api.GetWords()
+            # words = api.GetComponentImages(level=RIL.WORD, text_only=False, raw_image=True, raw_padding=5)
+            # line_data['words'] = []
+            # for iw, [image, bbox, _, _] in enumerate(words):
+            #     with open(os.path.join(odir, f'line-{idx}-word{iw}.jpg'), 'wb') as f:
+            #         image.save(f)
+            #     api.SetImage(image)
+            #     word = api.GetUTF8Text()
+            #     line_data['words'].append({
+            #         'text': word,
+            #         'bbox': bbox
+            #     })
+
+            data['lines'].append(line_data)
+
+        return data
+
+
+def iterate_words(img):
+    with PyTessBaseAPI(psm=PSM.AUTO, oem=OEM.LSTM_ONLY) as api:
+        api.SetImageFile(img)
+        text = api.GetUTF8Text()
+        data = {'text': text, 'words': []}
+        api.Recognize()
+        ri = api.GetIterator()
+        level = RIL.WORD
+        for r in iterate_level(ri, level):
+            word = r.GetUTF8Text(level)
+            conf = r.Confidence(level)
+            bbox = r.BoundingBox(level)
+            print(conf, word, bbox)
+            data['words'].append({'text': word, 'bbox': bbox})
+        return data
+
+
+def iterate_lines(img):
+    with PyTessBaseAPI(psm=PSM.AUTO, oem=OEM.LSTM_ONLY) as api:
+        api.SetImageFile(img)
+        text = api.GetUTF8Text()
+        data = {'text': text, 'lines': []}
+        api.Recognize()
+        ri = api.GetIterator()
+        level = RIL.TEXTLINE
+        for r in iterate_level(ri, level):
+            line = r.GetUTF8Text(level)
+            conf = r.Confidence(level)
+            bbox = r.BoundingBox(level)
+            print(conf, line, bbox)
+            data['lines'].append({'text': line, 'bbox': bbox})
+        return data
+
 
 if __name__ == '__main__':
     idir = 'raw-imgs'
@@ -9,8 +80,13 @@ if __name__ == '__main__':
 
     with PyTessBaseAPI(psm=PSM.AUTO, oem=OEM.LSTM_ONLY) as api:
         for img in glob.glob(os.path.join(idir, '*.png')):
-            api.SetImageFile(img)
-            with open(os.path.join(odir, f'ocr-{os.path.basename(img)}.txt'), 'w', encoding='utf-8') as f:
-                f.write(api.GetUTF8Text())
-            print(api.AllWordConfidences())
+
+            # data = iterate_words(img)
+
+            data = iterate_lines(img)
+
+            with open(os.path.join(odir, 'all_lines.json'), 'w') as f:
+                json.dump(data, f, indent=4)
+
+            break
 
